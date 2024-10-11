862352903317245e69de20cc4ccf82b3e249573d
IsidroTrevino <A00837104@tec.mx>
2024-10-11 11:21:02 -0600

    comentario sobre el patron de diseño del codigo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import recibosRoute from './routes/recibos.routes.js'
 
 const app = express()
 
+/*
+    La estructura del codigo de esta api se basa en el patron de diseño por capas, esto es debido a que el api esta 
+    organizada en capas separadas, cada una con una responsabilidad especifica, esto permite que el codigo sea mas 
+    facil de mantener y de entender, ademas de que se puede reutilizar facilmente. Como se puede observar las dos 
+    capas principales son la capa de usuarios y la capa de recibos, cada una de manejar informacion de usuarios y
+    recibos respectivamente.
+*/
+
 app.use(express.json())
 
 app.use('/api', usuarioRoute)
